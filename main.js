419a4e20d05ada6191c3b608525f7931eee17f28
Tim Kwok <timkwok808@gmail.com>
2024-04-17 23:45:36 -0700

    buttons added

---
+++ main.js
@@ -0,0 +1,22 @@
+sum = 0
+
+
+function calculate(){
+
+}
+
+function add(num1, num2){
+    sum = num1+num2
+}
+
+function subtract(){
+    
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
