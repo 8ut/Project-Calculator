358f7c97dc747d526342a4240faa15687efacb5e
Tim Kwok <timkwok808@gmail.com>
2024-05-14 07:45:36 -0700

    precedence checker added

--- main.js
+++ main.js
@@ -12,33 +12,54 @@ function clearDisplay() {
 var operandStack = []
 var operaterStack = []
 
-var split = display.value.split(" ")
+var split = display.value.split("")
 console.log(split)
+
+
+
 
 function calculate() {
     var split = display.value.split("")
     console.log(split)
     console.log(split.length)
 
-    for(let i = 0; i< split.length; i++){
-        if(!isNaN(split[i])){
+    for (let i = 0; i < split.length; i++) {
+        if (!isNaN(split[i])) {
             console.log(operaterStack.push(split[i]))
             var val = operaterStack.pop()
             console.log(val + "num")
-        }else if(isNaN(split[i] &&  )){
+        } else if (isNaN(split[i] && operandStack.length === 0)) {
             operandStack.push(split[i])
-            var op = operandStack.pop()
-            console.log(op + "operand")
-        }
+        } else if (isNaN(split[i] && operandStack.length != 0)) {
 
+        } else {//if empty
+            console.log("error")
+        }
+        console.log(operandStack)
     }
 }
 
 operandStack.pop()
 
-function simplify(){
+function simplify() {
 
 }
 
+
+function precedenceCheck(op) {
+    switch (op) {
+
+        case '+':
+        case '-':
+            return 0;
+            case "*":
+                case "/":
+                    return 1;
+
+
+    }
+}
+
+console.log(precedenceCheck("*") + "hello")
 
 
